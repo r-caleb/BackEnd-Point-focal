15f8e796896b71309fafd1f170112980925219e3
unknown <calebakasa7@gmail.com>
2025-07-06 21:42:58 +0100

    fix

--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const applicationController = require("../controllers/applicationController");
 const { protect } = require("../middleware/authMiddleware");
 
-// Route pour obtenir la liste des applications
-router.get("/", protect, applicationController.getApplications);
+
 
 // Route pour ajouter une application
 router.post("/", protect, applicationController.addApplication);
+
+// Route pour obtenir la liste des applications
+router.get("/", protect, applicationController.getApplications);
 
 // Route pour modifier une application
 router.put("/:id", protect, applicationController.updateApplication);
